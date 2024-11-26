4faa2fe7ef3948225a2c1e5d46de2c1ef205640f
marcos <marcgabe@myuax.com>
2024-11-26 19:15:40 +0100

    Initial commit

---
+++ src/test/java/org/example/backendwakandaculturaocioturismo/BackendWakandaCulturaOcioTurismoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendwakandaculturaocioturismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWakandaCulturaOcioTurismoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
